3ebbfe69043d9981d72b746bdab8ab990ed4fba9
“OnyekachiSamuel” <ezeokeonyekachi@gmail.com>
2018-02-23 14:17:47 +0100

    rework NWNotification[mh] conversion to swift

--- Source/Notification.swift
+++ Source/Notification.swift
@@ -2,15 +2,19 @@ import Foundation
 
 public class Notification: NSObject {
 
-    public var payload: String?
-    public var payloadData: Data?
-    public var token: String?
-    public var tokenData: Data?
-    public var identifier: UInt
+    public var addExpiration: Bool
     public var expiration: Date?
     public var expirationStamp: UInt
+    public var identifier: UInt
+    public var payload: String?
+    public var payloadData: Data?
     public var priority: UInt
-    public var addExpiration: Bool
+    public var token: String?
+    public var tokenData: Data?
+
+    private let deviceTokenSize: UInt = 32
+    private let payloadMaxSize: UInt = 256
+
 
     public init(payload: String,
                 token: String,
@@ -40,18 +44,96 @@ public class Notification: NSObject {
         self.priority = priority
     }
 
-    public func data(with type: NotificationType) -> Data {
-        return Data()
-    }
 
     public class func data(fromHex hex: String) -> Data {
-        return Data()
+
+        var result = Data()
+
+        hex.utf8.forEach { (scalarValue) in
+            var scalarUInt8 = UInt8(scalarValue)
+            result.append(&scalarUInt8, count: 1)
+        }
+        return result
     }
 
     public class func hex(from data: Data) -> String {
-        return ""
+
+        guard let stringValue = data.utf8String
+            else { return ""}
+
+        return stringValue
     }
 
-    private let deviceTokenSize: UInt = 32
-    private let payloadMaxSize: UInt = 256
+    public func data(with type: NotificationType) -> Data {
+
+//        switch type {
+//        case .type0:
+//            return
+//        default:
+//            <#code#>
+//        }
+        return Data()
+    }
+
+    private func filter(_ hex: String) -> String {
+
+        let hexToLowerCase = hex.lowercased()
+        var result = String()
+        for index in 0..<hex.count {
+            let charIndex = hexToLowerCase.index(hexToLowerCase.startIndex,
+                                      offsetBy: index)
+            let char = hexToLowerCase[charIndex]
+            if (char == "a" && char <= "f") || (char >= "0" && char <= "9") {
+
+                result.append(char)
+            }
+        }
+
+        return result
+    }
+
+    private func getPayload() -> String? {
+        guard let payload = payloadData else {
+            return nil
+        }
+
+        let string = String(data: payload, encoding: .utf8)
+
+        return string
+    }
+
+    private func setPayload(_ payload: String) {
+
+
+    }
+
+
+    private func dataWithType0() -> Data {
+
+        var size = (UInt8.bitWidth / 8)
+        size += (UInt32.bitWidth / 8) * 2
+        size += (UInt16.bitWidth / 8)
+        size += Int((deviceTokenSize))
+        size += (UInt16.bitWidth / 8)
+        size += Int((payloadMaxSize))
+
+        let command: UInt8 = 0
+
+
+        return Data()
+    }
+
+
+}
+
+extension Data {
+
+    var utf8String: String? {
+        return string(as: .utf8)
+    }
+
+    func string(as encoding: String.Encoding) -> String? {
+        return String(data: self, encoding: encoding)
+    }
+
 }

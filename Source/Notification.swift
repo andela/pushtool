ede2fcf0e0773c406cb849a5db47b3ac4baff642
J. G. Pusey <john.pusey@andela.com>
2018-03-12 11:14:33 -0400

    Some additional refactoring

--- Source/Notification.swift
+++ Source/Notification.swift
@@ -1,8 +1,6 @@
 import Foundation
 
 public class Notification {
-    public let addExpiration: Bool
-    public let expirationStamp: UInt
     public let priority: UInt
     public let payload: String
     public let token: String
@@ -16,21 +14,13 @@ public class Notification {
     public init(payload: String,
                 token: String,
                 identifier: UInt,
-                expiration: Date?,
+                expires: Date?,
                 priority: UInt) {
-        if let expiration = expiration {
-            self.addExpiration = true
-            self.expirationStamp = UInt(expiration.timeIntervalSince1970)
-        } else {
-            self.addExpiration = false
-            self.expirationStamp = 0
-        }
-
         self.identifier = identifier
         self.priority = priority
         self.payload = payload
         self.token = token
-        self.expires = expiration
+        self.expires = expires
     }
 
     private static func data(from hex: String) -> Data {
@@ -38,9 +28,9 @@ public class Notification {
     }
 
     public func data() -> Data {
-        var expires: UInt32 = UInt32(expirationStamp).bigEndian
-        var identifier: UInt32 = UInt32(self.identifier).bigEndian
-        var priority = self.priority
+        var identifier = UInt32(self.identifier).bigEndian
+        var priority = UInt8(self.priority)
+
         let normal = Notification.filter(token)
         let result = NSMutableData()
         let trunk = normal.count >= 64 ? String(normal.prefix(64)) : ""
@@ -52,12 +42,14 @@ public class Notification {
         result.append(&length, length: 4)
 
         let tokenData = Notification.data(from: trunk) as NSData
+
         append(to: result,
                identifier: 1,
                bytes: tokenData.bytes,
                length: tokenData.length)
 
         let payloadData = (payload.data(using: .utf8) ?? Data()) as NSData
+
         append(to: result,
                identifier: 2,
                bytes: payloadData.bytes,
@@ -70,7 +62,9 @@ public class Notification {
                    length: 4)
         }
 
-        if addExpiration {
+        if let expires = self.expires {
+            var expires = UInt32(expires.timeIntervalSince1970).bigEndian
+
             append(to: result,
                    identifier: 4,
                    bytes: &expires,
